a3f2e86501bae9504c9278f51c71d2210993dc83
perezd <derek@panoctagon.com>
2009-02-26 22:23:39 -0800

    Much happier with the parser now. Its a simplistic design, but much more functional which will allow for a more robust swapping strategy. Added a new way to add rules to the engine in realtime. Also, encapsulated out the yaml loader, so you can pass either a file or a string to the respective functions.

--- lib/smart_month/rulesets.rb
+++ lib/smart_month/rulesets.rb
@@ -3,42 +3,95 @@ require 'yaml'
 module SmartMonth
   class Rulesets
     
-    def self.load_file(path)
-      @@ruleset_yml = YAML::load(File.open(path))
-      self.new.activate_rules!
+    def self.load_yaml(yaml)
+      @@rulesets = YAML::load(yaml)
+      self.new.send(:activate_rules!)
     end
-        
+    
+    def self.load_file(path)
+      self.load_yaml(File.open(path))
+    end
+    
+    def self.add_rule(name, rule)
+      if rule.is_a?(String)
+        @@rulesets.merge!( { name => {'when' => rule } } ) 
+        self.new.send(:activate_rules!)
+      end
+    end
+  
+    protected 
+    
+    # iterates through all the rules currently defined in the class and injects them
+    # if they are not currently implemented.
     def activate_rules!
-      @@ruleset_yml.each_key do |rule|
-        inject_rule(rule, @@ruleset_yml[rule])
+      @@rulesets.each_key do |rule|
+        inject_rule(rule, @@rulesets[rule]) unless Month.respond_to?(rule)
       end
     end
     
-    protected 
+    # this method parses and defines the methods in our pre-existing ruby classes
+    def inject_rule(name,rule)
+      meth_name = name.gsub(' ','_').downcase
+      type, data = parse_string_as_date(rule) # ||= parse_string_as_frequency(rule)
+      # don't define the rulset methods if they might override existing methods!
+      define_methods(type,meth_name,data) unless Month.respond_to?(meth_name) && Date.respond_to?(meth_name)
+    end
     
+    # this is the parsing process for basic "date as string" rules
+    # (eg: 'December 12th, 2005' or 'March 15th')
     def parse_string_as_date(rule)
       # first attempt to parse the string and load structure
       month,day,year = rule['when'].split
       year = Time.now.year.to_i if year.nil?
       data = {:year => year.to_i, :month => Month.new(month).to_i, :day => day.to_i}
-      return data
-    end
-
-    def inject_rule(name,rule)
-      meth_name = name.gsub(' ','_').downcase
-      data = parse_string_as_date(rule)
-      # don't define the rulset methods if they might override existing methods!
-      unless Month.respond_to?(meth_name) && Date.respond_to?(meth_name)
-        # define root definition for lookup
-        Month.send(:define_method,meth_name) {
-          (self.month == data[:month]) ? Date.new(data[:year],data[:month],data[:day]) : nil
-        }
-        # alias root definition as boolean
-        Date.send(:define_method, "is_#{meth_name}?") {
-          self == Month[data[:month]].send(meth_name)
-        }
-      end
+      return [:date,data]
     end
     
+    def parse_string_as_frequency(rule)
+      raise "not defined!"
+      # return [:freq, data]
+    end    
+    
+    private
+    
+    # do not define the alias unless the root is defined properly
+    def define_methods(type,meth_name,data)
+      define_alias(meth_name,data) if define_root(type,meth_name,data)
+    end
+    
+    # define root definition for lookup directly into the Month class
+    def define_root(type,meth_name,data)
+      begin
+        Month.send(:define_method,meth_name) {
+          # algorithm template
+          origin = "(self.month == #{data[:month]})"
+          fail = 'nil'
+          # determine lookup based on requested strategy
+          case type
+            when :date: qry = "Date.new(#{data[:year]},#{data[:month]},#{data[:day]})"
+            when :freq: qry = 'true'
+            else qry = 'nil'
+          end
+          # evaluate the custom rule into the function
+          eval "#{origin} ? #{qry} : #{fail}"
+        }
+      rescue
+        false
+      end
+      true
+    end
+    
+    # alias root definition as boolean directly into the Date class
+    def define_alias(meth_name,data)
+      begin
+        Date.send(:define_method, "is_#{meth_name}?") {
+          [Month[data[:month]].send(meth_name)].flatten.include?(self)
+        }
+      rescue
+        false
+      end
+      true
+    end
+  
   end
 end
--- test/unit/rulesets_test.rb
+++ test/unit/rulesets_test.rb
@@ -7,14 +7,33 @@ class RulesetsTest < Test::Unit::TestCase
     SmartMonth::Rulesets.load_file(File.dirname(__FILE__)+'/samples/test_ruleset.yml')
   end
   
-  # tested By: Derek Perez
-  def test_should_return_date_from_string
-    # floating year example
+  # tested by: Derek Perez
+  def test_should_return_date_from_string_with_floating_year
     floating_year = {:year => Time.now.year, :month => 12, :day => 25}
-    assert_equal floating_year, SmartMonth::Rulesets.new.send(:parse_string_as_date,{'when' => "December 25th"})
-    # fixed year example
-    floating_year = {:year => 2010, :month => 12, :day => 25}
-    assert_equal floating_year, SmartMonth::Rulesets.new.send(:parse_string_as_date,{'when' => "December 25th, 2010"})
+    assert_equal [:date,floating_year], SmartMonth::Rulesets.new.send(:parse_string_as_date,{'when' => "December 25th"})
   end
+
+  # tested by: Derek Perez
+  def test_should_return_date_from_string_with_fixed_year
+    fixed_year = {:year => 2010, :month => 12, :day => 25}
+    assert_equal [:date,fixed_year], SmartMonth::Rulesets.new.send(:parse_string_as_date,{'when' => "December 25th, 2010"})
+  end
+  
+  # tested By: Derek Perez
+  def test_should_insert_rule_manually    
+    SmartMonth::Rulesets.add_rule('My Manual Rule', 'March 25th 2009')
+    assert_equal Month[3].my_manual_rule, Date.new(2009,3,25)
+  end
+  
+  def test_should_override_rule_definition_manually
+  end
+
+
+  def test_should_return_frequency_from_string
+  end
+  
+  def test_should_not_apply_previously_defined_rules
+  end
+  
   
 end
